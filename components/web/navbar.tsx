de38465502526805c9d0864cf95cf4bb0665e3b4
Aditya Kumar <aditya8799singh.com>
2026-03-18 12:05:09 +0530

    mostly done ready to deploy

--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -6,10 +6,11 @@ import { useConvexAuth } from "convex/react"
 import { authClient } from "@/lib/auth-client"
 import { toast } from "sonner"
 import { useRouter } from "next/navigation"
+import { SearchInput } from "./searchInput"
 
 export function Navbar() {
     const { isAuthenticated, isLoading } = useConvexAuth()
-    const router=useRouter();
+    const router = useRouter();
     return (
         <nav className="w-full py-5 flex items-center justify-between">
             <div className="flex items-center gap-8 ">
@@ -25,19 +26,23 @@ export function Navbar() {
                 </div>
             </div>
             <div className="flex items-center gap-2">
-                {isLoading ? null : isAuthenticated ? (<Button onClick={()=>authClient.signOut({
-                    fetchOptions:{onSuccess:()=>{
-                        toast.success("Logged out successfully")
-                        router.push('/')
-                    },
-                    onError:(error)=>{
-                        toast.error(error.error.message)
+                <div className="hidden md:block mr-2">
+                    <SearchInput />
+                </div>
+                {isLoading ? null : isAuthenticated ? (<Button onClick={() => authClient.signOut({
+                    fetchOptions: {
+                        onSuccess: () => {
+                            toast.success("Logged out successfully")
+                            router.push('/')
+                        },
+                        onError: (error) => {
+                            toast.error(error.error.message)
+                        }
                     }
-                }
                 })}>Logout</Button>) :
                     (<><Link href="/auth/sign-up" className={buttonVariants()}>Sign up</Link>
                         <Link href="/auth/login" className={buttonVariants({ variant: "outline" })}>Login</Link>
-                        </>)
+                    </>)
                 }
                 <ModeToggle />
             </div>
