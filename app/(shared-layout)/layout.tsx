d91742951a186af0bcef5beafc6ae4b51d3fb8fc
Aditya Kumar <aditya8799singh.com>
2026-03-19 15:44:05 +0530

    fix

--- app/(shared-layout)/layout.tsx
+++ app/(shared-layout)/layout.tsx
@@ -4,8 +4,6 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth";
 
-export const dynamic = "force-dynamic";
-
 export default async function SharedLayout({
   children,
 }: {
